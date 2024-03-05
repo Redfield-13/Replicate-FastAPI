57315b1d47379969ce0e6237bec9033e927f8a73
unknown <fieldr62@gmail.com>
2024-03-04 21:35:19 -0800

    MusicGen model route added

--- server.py
+++ server.py
@@ -47,3 +47,26 @@ def read_image(url: Union[str, None] = None, prompt: Union[str, None] = None):
             }
     )
     return {"message":output}
+
+
+@app.get("/musicgen")
+def read_image(model: Union[str, None] = None, prompt: Union[str, None] = None, duration: Union[int, None] = None):
+    output = replicate.run(
+    "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
+    input={
+        "top_k": 250,
+        "top_p": 0,
+        "prompt": prompt,
+        "duration": duration,
+        "temperature": 1,
+        "continuation": False,
+        "model_version": model,
+        "output_format": "wav",
+        "continuation_start": 0,
+        "multi_band_diffusion": False,
+        "normalization_strategy": "peak",
+        "classifier_free_guidance": 3
+        }
+    )
+    print(output)
+    return {"message":output}
