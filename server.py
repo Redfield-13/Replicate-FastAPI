7619395f3f18e348ac98c2196c3be039e32eab93
unknown <fieldr62@gmail.com>
2024-03-05 23:52:43 -0800

    more params added to routes

--- server.py
+++ server.py
@@ -13,15 +13,15 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/llava")
-def read_image(url: Union[str, None] = None, prompt: Union[str, None] = None):
+def read_image(url: Union[str, None] = None, prompt: Union[str, None] = None, top_p: Union[float, None] = None, temperature: Union[float, None] = None, max_tokens: Union[int, None] = None):
     output = replicate.run(
         "yorickvp/llava-13b:a0fdc44e4f2e1f20f2bb4e27846899953ac8e66c5886c5878fa1d6b73ce009e5",
         input={
             "image":url,
-            "top_p": 1,
+            "top_p": top_p,
             "prompt":prompt ,
-            "max_tokens": 1024,
-            "temperature": 0.2
+            "max_tokens": max_tokens,
+            "temperature": temperature/100
         }
     )
     message = ""
@@ -32,18 +32,18 @@ def read_image(url: Union[str, None] = None, prompt: Union[str, None] = None):
 
 
 @app.get("/photomaker")
-def read_image(url: Union[str, None] = None, prompt: Union[str, None] = None):
+def read_image(url: Union[str, None] = None, prompt: Union[str, None] = None, negprompt: Union[str, None] = None, style_name: Union[str, None] = None, num_steps: Union[int, None] = None, style_ratio: Union[int, None] = None, num_output: Union[int, None] = None, guide: Union[int, None] = None, seed: Union[int, None] = None):
     output = replicate.run(
         "tencentarc/photomaker:ddfc2b08d209f9fa8c1eca692712918bd449f695dabb4a958da31802a9570fe4",
         input={
             "prompt":"img" + prompt,
-            "num_steps": 50,
-            "style_name": "Photographic (Default)",
+            "num_steps": num_steps,
+            "style_name": style_name,
             "input_image": url,
-            "num_outputs": 1,
-            "guidance_scale": 5,
-            "negative_prompt": "nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry",
-            "style_strength_ratio": 20
+            "num_outputs": num_output,
+            "guidance_scale": guide,
+            "negative_prompt": negprompt,
+            "style_strength_ratio": style_ratio
             }
     )
     return {"message":output}
@@ -52,21 +52,21 @@ def read_image(url: Union[str, None] = None, prompt: Union[str, None] = None):
 @app.get("/musicgen")
 def read_image(model: Union[str, None] = None, prompt: Union[str, None] = None, duration: Union[int, None] = None):
     output = replicate.run(
-    "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
-    input={
-        "top_k": 250,
-        "top_p": 0,
-        "prompt": prompt,
-        "duration": duration,
-        "temperature": 1,
-        "continuation": False,
-        "model_version": model,
-        "output_format": "wav",
-        "continuation_start": 0,
-        "multi_band_diffusion": False,
-        "normalization_strategy": "peak",
-        "classifier_free_guidance": 3
-        }
+        "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
+        input={
+            "top_k": 250,
+            "top_p": 0,
+            "prompt": prompt,
+            "duration": duration,
+            "temperature": 1,
+            "continuation": False,
+            "model_version": model,
+            "output_format": "wav",
+            "continuation_start": 0,
+            "multi_band_diffusion": False,
+            "normalization_strategy": "peak",
+            "classifier_free_guidance": 3
+            }
     )
     print(output)
-    return {"message":output}
+    return {"link":output}
